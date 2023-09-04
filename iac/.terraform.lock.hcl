ac3efbe0c5d7126eb3a9470d1ecd5c9e509529f6
Damien Vergnaud <damien.vergnaud@wescale.fr>
2023-09-04 19:44:52 +0200

    feat(CI&Terraform): AutoPlan for Terraform & Deploy stack for tests

---
+++ iac/.terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/scaleway/scaleway" {
+  version = "2.27.0"
+  hashes = [
+    "h1:Og47Om5NlmVBxIowPf2eqTtnCB+eHmcdocIugrBxoAI=",
+    "zh:69703b786535ac43a47145e48875f7dd5da2de9b2868e35cb3053374f912cf5f",
+    "zh:75a62a02971545c216732b369d78cf277d4065e23d8ff453bd3ddc37ff87a2eb",
+    "zh:8bd985631980f60e96f197be254bee6ef7b431169025d615703d12eee74f8119",
+    "zh:910012fcded93f99768a5b66aba7bcd94a3fd94ffe95fdb2cc96b11b9cc3643c",
+    "zh:917b46a52647767afaa169abb6c2ddab4274b3fbcfa47ae66670335bd26c5a5c",
+    "zh:91a1ffc2c219a2db0226dc43a6279c85708d1d62ab4f2dbd4b59c6f43d7d38ab",
+    "zh:ab08103d5fff1817e295754ad8684883eb0652be89116a260f64db166a6b430b",
+    "zh:b972c3940f4fd0ae45b5ff6dee15fcd0f2065db3f51d8fe9b6b03825ff812ae3",
+    "zh:bac194f28d24a72499b2e92e3126475de1f6bd57709f972b5f2120e6bfc7ef3b",
+    "zh:bea0c0a52d2e6fdd2bb259c5bbfedd481a3117cbc480dcf832abb8dd169fd4d6",
+    "zh:e617974d3af9ac1d0eb9d9cbc9595342474e9ddf374e27d3e39554489cd0d10e",
+    "zh:eeca97061dbf09c887347467cd3ea2f41839550f8a47695f571ba721b6d18079",
+    "zh:fc521229e8a83b6e0bf4d65452484e242cf7eef073e4c63abe793934bebe4f7c",
+    "zh:fdc15b5e46fafb82acd5e3b0f1dda1743686b98d5c6ba40e34e91bae67f00c91",
+  ]
+}
---
+++ iac/main.tf
@@ -0,0 +1,89 @@
+###############################################
+#        SETUP THE SCALEWAY PROVIDER          #
+###############################################
+
+terraform {
+  required_providers {
+    scaleway  = {
+      source  = "scaleway/scaleway"
+      version = "= 2.27.0"
+    }
+  }
+  required_version = "= 1.5.6"
+}
+
+
+###############################################
+#         CONFIGURE THE K8S CLUSTER           #
+###############################################
+
+resource "scaleway_k8s_cluster" "multicloud" {
+  name    = "multicloud-cluster"
+  type    = "multicloud"
+  version = "1.27.1"
+  cni     = "kilo"
+  region  = "fr-par"
+  delete_additional_resources = false
+}
+
+resource "scaleway_k8s_pool" "pool" {
+  cluster_id  = scaleway_k8s_cluster.multicloud.id
+  name        = "multicloud-pool"
+  node_type   = "external"
+  size        = 0
+  region      = "fr-par"
+}
+
+###############################################
+#     CONFIGURE THE ELASTIC METAL SERVER      #
+###############################################
+
+# Select at least one SSH key to connect to your server
+resource "scaleway_iam_ssh_key" "key" {
+  name = "hello-remote-node"
+  public_key = file(".id_rsa.pub")
+}
+# Select the type of offer for your server
+data "scaleway_baremetal_offer" "offer" {
+  name = "EM-B112X-SSD"
+}
+# Select the OS you want installed on your server
+data "scaleway_baremetal_os" "os" {
+  name = "Ubuntu"
+  version = "20.04 LTS (Focal Fossa)"
+}
+
+resource "scaleway_baremetal_server" "server" {
+  offer       = data.scaleway_baremetal_offer.offer.name  # The name of the Elastic Metal offer
+  os          = data.scaleway_baremetal_os.os.id          # The ID of the OS
+  ssh_key_ids = [scaleway_iam_ssh_key.key.id]             # The list of SSH key IDs allowed to connect to the server
+  zone        = "fr-par-2"
+}
+
+
+data "local_sensitive_file" "secret_key" {
+    filename = pathexpand(".id_rsa.pkey")
+}
+
+resource "scaleway_baremetal_server" "server" {
+    offer       = data.scaleway_baremetal_offer.offer.name
+    os          = data.scaleway_baremetal_os.os.id
+    ssh_key_ids = [scaleway_iam_ssh_key.key.id]
+
+    # Configure the SSH connexion used by Terraform for the remote execution  
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      host     = one([for k in self.ips : k if k.version == "IPv4"]).address   # We look for the IPv4 in the list of IPs
+    }
+
+    # Download and execute the configuration script
+    provisioner "remote-exec" {
+      inline = [
+        "wget https://scwcontainermulticloud.s3.fr-par.scw.cloud/node-agent_linux_amd64 > log && chmod +x node-agent_linux_amd64",
+        "echo \"\nPOOL_ID=${split("/", scaleway_k8s_pool.pool.id)[1]}\nPOOL_REGION=${scaleway_k8s_pool.pool.region}\nSCW_SECRET_KEY=${data.local_sensitive_file.secret_key.content}\" >> log",
+        "export POOL_ID=${split("/", scaleway_k8s_pool.pool.id)[1]}  POOL_REGION=${scaleway_k8s_pool.pool.region}  SCW_SECRET_KEY=${data.local_sensitive_file.secret_key.content}",
+        "sudo ./node-agent_linux_amd64 -loglevel 0 -no-controller >> log",
+      ]
+    }
+}
